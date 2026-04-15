39859cedd538a3783e7ccbb2cc75c473079d5e60
Pradumn <pradumn.shukla@incaendo.com>
2026-04-15 13:07:18 +0530

    removed env vars

--- app/src/awsConfig.ts
+++ app/src/awsConfig.ts
@@ -1,15 +1,11 @@
 import { Amplify } from 'aws-amplify';
 
-const region =
-  process.env.EXPO_PUBLIC_AWS_REGION || process.env.AWS_REGION || 'ap-south-1';
+const region = process.env.EXPO_PUBLIC_AWS_REGION || process.env.AWS_REGION;
 const userPoolId =
-  process.env.EXPO_PUBLIC_AWS_USER_POOL_ID ||
-  process.env.AWS_USER_POOL_ID ||
-  'ap-south-1_mtAI2LLNj';
+  process.env.EXPO_PUBLIC_AWS_USER_POOL_ID || process.env.AWS_USER_POOL_ID;
 const userPoolWebClientId =
   process.env.EXPO_PUBLIC_AWS_USER_POOL_CLIENT_ID ||
-  process.env.AWS_USER_POOL_CLIENT_ID ||
-  '6fsf4rjptgcphmb7kkekhp1634';
+  process.env.AWS_USER_POOL_CLIENT_ID;
 
 Amplify.configure({
   Auth: {
